084c02f678e728532405d09c352276875bfe1ffc
PabloOsorix <3387@holbertonschool.com>
2022-06-09 18:03:45 -0500

    :heavy_minus_sign: remove comments

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,6 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  alert("DOM ready!");
-});
 function removeId(array, value) {
   var index = 0;
   while (index < array.length) {
@@ -16,55 +13,18 @@ var amenities = [];
 var select_amt = []
 var counter = 0;
 const checkboxes = document.querySelectorAll("input[name=check]").forEach(function(element){
-  element.addEventListener('click', function() {
-    var id = element.getAttribute("data-id")
-    var name = element.getAttribute("data-name")
-    if (this.checked){
-      amenities.push(id)
-      select_amt.push(name)
-      window.alert("It's check " + amenities)
-      document.getElementById("select_amt").textContent = select_amt
-    } else {
-      removeId(amenities, id)
-      removeId(select_amt, name)
-      window.alert(amenities)
-      document.getElementById("select_amt").textContent = select_amt
-    }
-  });
+element.addEventListener('click', function() {
+  var id = element.getAttribute("data-id")
+  var name = element.getAttribute("data-name")
+  if (this.checked){
+    amenities.push(id)
+    select_amt.push(` ${name}`)
+    document.getElementById("select_amt").textContent = select_amt
+  } else {
+    name = ` ${name}`
+    removeId(amenities, id)
+    removeId(select_amt, name)
+    document.getElementById("select_amt").textContent = select_amt
+  }
+    });
 });
-
-
-// const checkboxes = document.querySelectorAll("input[name=check]").forEach(function(value){
-//   value.addEventListener('click', function() {
-//     if (this.checked){
-//       var data_name = this.value.getAttribute("data-name");
-//       var id = this.value.getAttribute("data-id");
-//       amenities[data_name] = [{"id": id}];
-//       window.alert(amenities[data_name]);
-//     } else {
-//       if (data_name == amenities.name){
-//         delete amenities[data_name];
-//         window.alert(amenities[data_name]);
-//     }
-//   }
-//   })
-// });
-
-
-//var amenities = {}
-//const checkboxes = document.querySelectorAll("input[name=check]");
-//checkboxes.forEach((value) =>
-//value.addEventListener('change', function() {
-//  var id = value.getAttribute("data-id")
-//  var data_name = value.getAttribute("data-name")
-//  if (this.checked) {
-//    amenities[data_name] = [{"id": id}];
-//    window.alert(amenities.getAttribute(data_name));
-//  } else {
-//      if (data_name == amenities.name){
-//        delete amenities[data_name];
-//        window.alert(amenities.id);
-//    }
-//  }
-//}));
-//
