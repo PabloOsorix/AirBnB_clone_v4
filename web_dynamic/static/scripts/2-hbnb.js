a2c06e4e2927bf7c6d281fe42b571d1513af383d
PabloOsorix <3387@holbertonschool.com>
2022-06-09 18:05:21 -0500

    :heavy_minus_sign: remove comments

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -12,26 +12,28 @@ function removeId(array, value) {
   }
   return array;
 }
-
+// POINT 2
 var amenities = [];
 var select_amt = []
 var counter = 0;
 const checkboxes = document.querySelectorAll("input[name=check]").forEach(function(element){
-  element.addEventListener('click', function() {
-    var id = element.getAttribute("data-id")
-    var name = element.getAttribute("data-name")
-    if (this.checked){
-      amenities.push(id)
-      select_amt.push(name)
-      document.getElementById("select_amt").textContent = select_amt
-    } else {
-      removeId(amenities, id)
-      removeId(select_amt, name)
-      document.getElementById("select_amt").textContent = select_amt
-    }
-  });
+element.addEventListener('click', function() {
+  var id = element.getAttribute("data-id")
+  var name = element.getAttribute("data-name")
+  if (this.checked){
+    amenities.push(id)
+    select_amt.push(` ${name}`)
+    document.getElementById("select_amt").textContent = select_amt
+  } else {
+    name = ` ${name}`
+    removeId(amenities, id)
+    removeId(select_amt, name)
+    document.getElementById("select_amt").textContent = select_amt
+  }
+    });
 });
 
+// POINT 3
 const div_circle = document.querySelector("div#api_status");
   fetch("http://localhost:5001/api/v1/status").then((response) => {
     if (response.status == 200) {
